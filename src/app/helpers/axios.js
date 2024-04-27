0e3fce899733c72a7989844b4004ae476c2b7149
3La221 <abacha3la2@gmail.com>
2024-04-27 14:56:39 +0100

    'authf'

--- src/app/helpers/axios.js
+++ src/app/helpers/axios.js
@@ -1,20 +1,53 @@
+import axios from "axios";
+import createAuthRefreshInterceptor from "axios-auth-refresh";
+
+const axiosService = axios.create({
+    baseURL : "https://ai2dz.com/CodeItBack/api/",
+    headers : {
+        "Content-Type":"application/json",
+    }
+})
 
 
-function logout(navigate) {
-    localStorage.removeItem("auth");
-    navigate("/login");
+
+axiosService.interceptors.request.use(async(config)=>{
+    const {access} = JSON.parse(localStorage.getItem("auth"));
+    config.headers.Authorization = `Bearer ${access}`
+    return config
+})
+
+axiosService.interceptors.response.use(
+    (res) => Promise.resolve(res) ,
+    (err) => Promise.reject(err)
+)
+
+const refreshAuthLogic = async(failedRequest) =>{
+    const data = JSON.parse(localStorage.getItem("auth"))
+    
+    const {refresh,user_id} = data 
+    return axios.post("/token/refresh/",data,{
+        baseURL : `https://ai2dz.com/CodeItBack/api/`,
+        headers :{
+            Authorization : `Bearer ${refresh}`
+        }
+    }).then((resp)=>{
+        const {access} = resp.data;
+        failedRequest.response.config.headers["Authorization"] = "Bearer" + access;
+        localStorage.setItem("auth",JSON.stringify({
+            access,refresh,user_id
+        })) 
+    })
+    .catch(()=>{
+        localStorage.removeItem("auth")
+    })
 }
 
 
-function getUser() {
-const auth = JSON.parse(localStorage.getItem("auth")) || null;
-if (auth) {
-    return auth.user_id;
-} else {
-    return null;
-}
-}
 
 
 
-export { getUser   , logout};
+createAuthRefreshInterceptor(axiosService,refreshAuthLogic) 
+
+
+
+export default axiosService
