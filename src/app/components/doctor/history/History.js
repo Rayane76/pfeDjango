022e087f709cb83d0800f9001de5977e05c4bcd5
Rayane <rayanehiouani.rh@gmail.com>
2024-05-02 19:18:33 +0100

    completed historique with filtering method

--- src/app/components/doctor/history/History.js
+++ src/app/components/doctor/history/History.js
@@ -1,14 +1,229 @@
-import "../../../styles/doctor/patient/historique.css"
-import HistoriqueSvg from "@/app/utils/svg/historique"
+"use client";
+import "../../../styles/doctor/patient/historique.css";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import HistoriqueSvg from "@/app/utils/svg/historique";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import Box from "@mui/material/Box";
+import { useState } from "react";
 
+export default function History() {
+  const maladies = [
+    {
+      nom: "maladie1",
+      date: "14-02-2012",
+      medecin: "Dr. Bendriss Asma",
+      categorie: "cardiaque",
+    },
+    {
+      nom: "maladie2",
+      date: "11-06-2012",
+      medecin: "Dr. Omar Fouad",
+      categorie: "renal",
+    },
+  ];
 
-export default function History(){
-    return(
-        <div className="historiqueDiv">
-            <div className='historiqueDivTitleDiv'>
-           <HistoriqueSvg />
-           <h1 className='historiqueDivTitle'>Historique</h1>
-           </div>
+  const [filteredCat,setFilteredCat] = useState(undefined);
+  const [filteredMed,setFilteredMed] = useState(undefined);
+
+  
+  const handleSwitch = (e, selected, button, otherBtn) => {
+    e.preventDefault();
+    const clickedOne = document.getElementById(selected);
+    const activeOne = document.getElementsByClassName("activeOneInHistorique");
+    const selectedbtn = document.getElementById(button);
+    const notSelected = document.getElementById(otherBtn);
+
+    if (clickedOne.classList.contains("unActive")) {
+      activeOne[0].classList.add("unActive");
+      activeOne[0].classList.remove("activeOneInHistorique");
+      clickedOne.classList.remove("unActive");
+      clickedOne.classList.add("activeOneInHistorique");
+
+      selectedbtn.classList.toggle("activeHistoriqueBtn");
+      selectedbtn.classList.toggle("notActiveHistoriqueBtn");
+
+      notSelected.classList.toggle("activeHistoriqueBtn");
+      notSelected.classList.toggle("notActiveHistoriqueBtn");
+    }
+  };
+
+  const handleChangeFilterCat = (e)=>{
+      console.log(e.target.innerText);
+      setFilteredCat(e.target.innerText);
+  }
+
+  const handleChangeFilterMed = (e)=>{
+     setFilteredMed(e.target.innerText);
+  }
+
+  return (
+    <div className="historiqueDiv">
+      <div className="historiqueDivTitleDiv">
+        <HistoriqueSvg />
+        <h1 className="historiqueDivTitle">Historique des maladies</h1>
+      </div>
+      <div className="historiquebtnsDiv">
+        <button
+          id="personnelBtn"
+          onClick={(e) =>
+            handleSwitch(
+              e,
+              "historiquePersonnel",
+              "personnelBtn",
+              "familialBtn"
+            )
+          }
+          className="historiqueBtns activeHistoriqueBtn"
+        >
+          Personnelles
+        </button>
+        <button
+          id="familialBtn"
+          onClick={(e) =>
+            handleSwitch(e, "historiqueFamilial", "familialBtn", "personnelBtn")
+          }
+          className="historiqueBtns notActiveHistoriqueBtn"
+        >
+          Familiaux
+        </button>
+      </div>
+      <div className="historiqueFilterDiv">
+        <Autocomplete
+        //   value={filteredCat}
+          onChange={(e)=>handleChangeFilterCat(e)}
+          disablePortal
+          id="combo-box-demo"
+          sx={{ width: 300 }}
+          options={maladies}
+          autoHighlight
+          getOptionLabel={(option) => option.categorie}
+          renderOption={(props, option) => (
+            <Box
+              component="li"
+              sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+              {...props}
+            >
+              {option.categorie}
+            </Box>
+          )}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Categorie"
+              inputProps={{
+                ...params.inputProps,
+                autoComplete: "new-password", // disable autocomplete and autofill
+              }}
+            />
+          )}
+        />
+        <Autocomplete
+          id="country-select-demo"
+          onChange={(e)=>handleChangeFilterMed(e)}
+          sx={{ width: 300 }}
+          options={maladies}
+          autoHighlight
+          getOptionLabel={(option) => option.medecin}
+          renderOption={(props, option) => (
+            <Box
+              component="li"
+              sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+              {...props}
+            >
+              {option.medecin}
+            </Box>
+          )}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Medecin"
+              inputProps={{
+                ...params.inputProps,
+                autoComplete: "new-password", // disable autocomplete and autofill
+              }}
+            />
+          )}
+        />
+      </div>
+      <div
+        id="historiquePersonnel"
+        className="historiquePersonnelDiv activeOneInHistorique"
+      >
+        <div className="tableTitleDivPersonnel">
+          <label className="tableTitleLabel">Nom</label>
+          <label className="tableTitleLabel">Date</label>
+          <label className="tableTitleLabel">Ajouté par</label>
+          <label className="tableTitleLabell">Catégorie</label>
         </div>
-    )
+        <div className="tableRowsPersonnel">
+          { filteredCat === undefined && filteredMed === undefined ?
+            maladies.map((maladie)=>{
+            return(
+                <div className="tableRowPers">
+            <label className="labelRowPers2">{maladie.nom}</label>
+            <label className="labelRowPers1">{maladie.date}</label>
+            <label className="labelRowPers">{maladie.medecin}</label>
+            <label className="labellast">{maladie.categorie}</label>
+          </div>
+            )
+            
+          }) : 
+           filteredCat != undefined && filteredMed === undefined ?
+           maladies.map((maladie)=>{
+            if(maladie.categorie === filteredCat){
+            return(
+                <div className="tableRowPers">
+            <label className="labelRowPers2">{maladie.nom}</label>
+            <label className="labelRowPers1">{maladie.date}</label>
+            <label className="labelRowPers">{maladie.medecin}</label>
+            <label className="labellast">{maladie.categorie}</label>
+          </div>
+            )
+            }
+        }) : 
+            filteredCat === undefined && filteredMed != undefined ?
+            maladies.map((maladie)=>{
+            if(maladie.medecin === filteredMed){    
+            return(
+                <div className="tableRowPers">
+            <label className="labelRowPers2">{maladie.nom}</label>
+            <label className="labelRowPers1">{maladie.date}</label>
+            <label className="labelRowPers">{maladie.medecin}</label>
+            <label className="labellast">{maladie.categorie}</label>
+          </div>
+            )
+            }
+        }) : 
+            maladies.map((maladie)=>{
+            if(maladie.categorie === filteredCat && maladie.medecin === filteredMed){    
+            return(
+                <div className="tableRowPers">
+            <label className="labelRowPers2">{maladie.nom}</label>
+            <label className="labelRowPers1">{maladie.date}</label>
+            <label className="labelRowPers">{maladie.medecin}</label>
+            <label className="labellast">{maladie.categorie}</label>
+          </div>
+            )
+            }
+          })
+          }
+        </div>
+      </div>
+      <div id="historiqueFamilial" className="historiqueFamilialDiv unActive">
+        <div className="tableTitleDivFamilial">
+          <label className="tableTitleLabel">Antécédent</label>
+          <label className="tableTitleLabel">Membre Famille</label>
+          <label className="tableTitleLabelF">Catégorie</label>
+        </div>
+        <div className="tableRowsFamilial">
+          <div className="tableRow">
+            <label className="labelRowFamilial2">Cancer du côlon</label>
+            <label className="labelRowFamilial1">Pere</label>
+            <label className="labelRowFamilial">Cardiaque</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/components/doctor/modify/Modify.js
+++ src/app/components/doctor/modify/Modify.js
@@ -1,44 +1,187 @@
-'use client'
+"use client";
 import DeleteIcon from "@mui/icons-material/Delete";
 import "../../../styles/doctor/patient/modify.css";
 import ModifyTitleSvg from "@/app/utils/svg/modifyTitle";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
 
 export default function Modify() {
+  const [selectedDiv, setSelectedDiv] = useState("modifyAllergies");
+  const [modalShowAllergie, setModalShowAllergie] = useState(false);
+  const [modalShowAnt, setModalShowAnt] = useState(false);
 
-
-   const handleSwitch = (e,selected,button,otherBtn)=>{
+  const handleSwitch = (e, selected, button, otherBtn) => {
     e.preventDefault();
     const clickedOne = document.getElementById(selected);
     const activeOne = document.getElementsByClassName("activeOneInModify");
     const selectedbtn = document.getElementById(button);
     const notSelected = document.getElementById(otherBtn);
+    setSelectedDiv(selected);
 
-    if(clickedOne.classList.contains("unActive")){
-        activeOne[0].classList.add("unActive");
-        activeOne[0].classList.remove("activeOneInModify");
-        clickedOne.classList.remove("unActive");
-        clickedOne.classList.add("activeOneInModify");
-        
-        selectedbtn.classList.toggle("activeModifyBtn");
-        selectedbtn.classList.toggle("notActiveModifyBtn");
-        
-        notSelected.classList.toggle("activeModifyBtn");
-        notSelected.classList.toggle("notActiveModifyBtn");
+    if (clickedOne.classList.contains("unActive")) {
+      activeOne[0].classList.add("unActive");
+      activeOne[0].classList.remove("activeOneInModify");
+      clickedOne.classList.remove("unActive");
+      clickedOne.classList.add("activeOneInModify");
 
+      selectedbtn.classList.toggle("activeModifyBtn");
+      selectedbtn.classList.toggle("notActiveModifyBtn");
+
+      notSelected.classList.toggle("activeModifyBtn");
+      notSelected.classList.toggle("notActiveModifyBtn");
     }
-   }
+  };
 
+  const handleAdd = (e) => {
+    if (selectedDiv === "modifyAllergies") {
+      setModalShowAllergie(true);
+    } else {
+      setModalShowAnt(true);
+    }
+  };
+
+  function MyVerticallyCenteredModalAllergie(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">Allergie</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Ajouter nouvelle allergie :</h4>
+          <input
+            className="me-4"
+            name="allergie"
+            type="text"
+            required
+            placeholder="allergie ..."
+          ></input>
+          <input className="me-2" type="checkbox"></input>
+          <label>Afficher sur la carte ?</label>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button>Ajouter</Button>
+          <Button variant="secondary" onClick={props.onHide}>
+            Annuler
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  function MyVerticallyCenteredModalAnt(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Antécédent
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Ajouter nouvel antécédent</h4>
+          <div className="d-flex flex-column gap-3">
+            <div className="d-flex">
+              <label className="me-2">Antécédent : </label>
+              <input
+                name="ant"
+                type="text"
+                required
+                placeholder="antecedent ..."
+              ></input>
+            </div>
+            <div className="d-flex">
+              <label className="me-2">Membre : </label>
+              <input
+                name="membre"
+                type="text"
+                required
+                placeholder="membre"
+              ></input>
+            </div>
+            <div className="d-flex">
+              <label className="me-2">Catégorie : </label>
+              <input
+                name="categore"
+                type="text"
+                required
+                placeholder="categorie"
+              ></input>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button>Ajouter</Button>
+          <Button variant="secondary" onClick={props.onHide}>
+            Annuler
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 
   return (
     <div className="modifyDiv">
       <div className="modifyDivTitleDiv">
-        <ModifyTitleSvg />
-        <h1 className="modifyDivTitle">Modifier les informations</h1>
+        <div className="d-flex justify-content-center align-items-center">
+          <ModifyTitleSvg />
+          <h1 className="modifyDivTitle">Modifier les informations</h1>
+        </div>
+        <button
+          onClick={(e) => handleAdd(e)}
+          title="Add"
+          className="cssbuttons-io-button"
+        >
+          <svg
+            height="25"
+            width="25"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path
+              d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          <span>Nouveau</span>
+        </button>
+        <MyVerticallyCenteredModalAllergie
+          show={modalShowAllergie}
+          onHide={() => setModalShowAllergie(false)}
+        />
+        <MyVerticallyCenteredModalAnt
+          show={modalShowAnt}
+          onHide={() => setModalShowAnt(false)}
+        />
       </div>
 
       <div className="modifybtnsDiv">
-        <button id="allergieBtn" onClick={(e)=>handleSwitch(e,"modifyAllergies","allergieBtn","antBtn")} className="modifyBtns activeModifyBtn">Allergies</button>
-        <button id="antBtn" onClick={(e)=>handleSwitch(e,"modifyAntecedents","antBtn","allergieBtn")} className="modifyBtns notActiveModifyBtn">
+        <button
+          id="allergieBtn"
+          onClick={(e) =>
+            handleSwitch(e, "modifyAllergies", "allergieBtn", "antBtn")
+          }
+          className="modifyBtns activeModifyBtn"
+        >
+          Allergies
+        </button>
+        <button
+          id="antBtn"
+          onClick={(e) =>
+            handleSwitch(e, "modifyAntecedents", "antBtn", "allergieBtn")
+          }
+          className="modifyBtns notActiveModifyBtn"
+        >
           Antécédents Familiaux
         </button>
       </div>
@@ -46,7 +189,7 @@ export default function Modify() {
         <div className="tableTitleDivAllergie">
           <label className="tableTitleLabel">Allergie</label>
           <label className="tableTitleLabel">Afficher sur la carte ?</label>
-          <label style={{height:"24px",width:"24px"}}></label>
+          <label style={{ height: "24px", width: "24px" }}></label>
         </div>
         <div className="tableRows">
           <div className="tableRow">
@@ -71,10 +214,10 @@ export default function Modify() {
           <label className="tableTitleLabel">Antécédent</label>
           <label className="tableTitleLabel">Membre Famille</label>
           <label className="tableTitleLabel">Catégorie</label>
-          <label style={{height:"24px",width:"24px"}}></label>
+          <label style={{ height: "24px", width: "24px" }}></label>
         </div>
         <div className="tableRowsAnt">
-        <div className="tableRow">
+          <div className="tableRow">
             <label className="labelRowAnt2">Cancer du côlon</label>
             <label className="labelRowAnt1">Pere</label>
             <label className="labelRowAnt">Cardiaque</label>
