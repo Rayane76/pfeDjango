813f783dfb884a42de71fb0fb0342a51b3f731b9
Rayane <rayanehiouani.rh@gmail.com>
2024-05-26 01:04:53 +0100

    reg

--- src/app/reg/page.js
+++ src/app/reg/page.js
@@ -1,91 +1,139 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/reg.css";
 
 export default function Reg() {
 
     const [currentStep, setCurrentStep] = useState(0);
 
+
+    const [allInfos,setAllInfos] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        numero_tel: "",
+        emergency_number: "",
+        birth_date: "",
+        gender: "",
+        blood_type: "",
+        address: "",
+        carte_id: "",
+        password: "",
+        role:"P",
+        situation: "",
+        nbr_enfants: "",
+    })
+
+    const handleInput = (e) => {
+        setAllInfos((prev)=>({...prev,[e.target.name]:e.target.value}));
+      }
+
+      const [cfrm,setCfrm] = useState("");
+
     const steps = [
       <div className="inputStep">
         <div className="oneInputDiv">
           <label className="label">Nom : </label>
-          <input className="input" />
+          <input required name="last_name" className="input" onChange={(e)=>handleInput(e)} />
         </div>
         <div className="oneInputDiv">
           <label className="label">Prénom : </label>
-          <input className="input" />
+          <input required name="first_name" className="input" onChange={(e)=>handleInput(e)} />
         </div>
         <div className="oneInputDiv">
           <label className="label">Email : </label>
-          <input className="input" />
+          <input required name="email" type="email" className="input" onChange={(e)=>handleInput(e)} />
         </div>
       </div>,
   
       <div className="inputStep">
         <div className="oneInputDiv">
           <label className="label">Adresse : </label>
-          <input className="input" />
+          <input required={currentStep === 1 ? true : false} name="address" className="input" onChange={(e)=>handleInput(e)} />
         </div>
         <div className="oneInputDiv">
           <label className="label">Numéro de téléphone : </label>
-          <input className="input" />
+          <input  required={currentStep === 1 ? true : false}  name="numero_tel" className="input" onChange={(e)=>handleInput(e)} />
         </div>
         <div className="oneInputDiv">
           <label className="label">Contact d'urgence : </label>
-          <input className="input" />
+          <input  required={currentStep === 1 ? true : false}  name="emergency_number" className="input" onChange={(e)=>handleInput(e)} />
         </div>
       </div>,
   
       <div className="inputStep">
         <div className="oneInputDiv">
           <label className="label">Date de naissance : </label>
-          <input className="input" />
+          <input  required={currentStep === 2 ? true : false}  name="birth_date" className="input" type="date" onChange={(e)=>handleInput(e)} />
         </div>
         <div className="oneInputDiv">
           <label className="label">Sexe : </label>
-          <input className="input" />
+          <select required={currentStep === 2 ? true : false} value={allInfos.gender} name="gender" className="input" onChange={(e)=>handleInput(e)}>
+            <option hidden>Choisir le sexe : </option>
+            <option value="male">Homme</option>
+            <option value="femele">Femme</option>
+          </select>
+
         </div>
         <div className="oneInputDiv">
           <label className="label">Groupe sanguin : </label>
-          <input className="input" />
+          <select required={currentStep === 2 ? true : false} name="blood_type" value={allInfos.blood_type} className="input" onChange={(e)=>handleInput(e)}>
+            <option hidden>Choisir le groupe sanguin : </option>
+            <option value="O+">O +</option>
+            <option value="O-">O -</option>
+            <option value="A+">A +</option>
+            <option value="A-">A -</option>
+            <option value="B+">B +</option>
+            <option value="B-">B -</option>
+            <option value="AB+">AB +</option>
+            <option value="AB-">AB -</option>
+          </select>
         </div>
       </div>,
   
       <div className="inputStep">
         <div className="oneInputDiv">
           <label className="label">Numéro d'identification national : </label>
-          <input className="input" />
+          <input required={currentStep === 3 ? true : false} name="carte_id" className="input" onChange={(e)=>handleInput(e)} />
         </div>
         <div className="oneInputDiv">
           <label className="label">Situation familiale : </label>
-          <input className="input" />
+          <select required={currentStep === 3 ? true : false} name="situation" value={allInfos.situation} className="input" onChange={(e)=>handleInput(e)}>
+            <option hidden>Choisir Situation familiale : </option>
+            <option value="celibataire">Célibataire</option>
+            <option value="mariee">Marié(e)</option>
+            <option value="divorcee">Divorcé(e)</option>
+            <option value="veuf">Veuf/Veuve</option>
+          </select>
         </div>
         <div className="oneInputDiv">
           <label className="label">Nombre d'enfants : </label>
-          <input className="input" />
+          <input required={currentStep === 3 ? true : false} onChange={(e)=>handleInput(e)} disabled={allInfos.situation === "" || allInfos.situation === "celibataire" ? true : false} name="nbr_enfants" type="number" className="input" />
         </div>
       </div>,
   
       <div className="inputStep">
         <div className="oneInputDiv">
           <label className="label">Mot de passe : </label>
-          <input className="input" />
+          <input required={currentStep === 4 ? true : false} onChange={(e)=>handleInput(e)} name="password" className="input" />
         </div>
         <div className="oneInputDiv">
           <label className="label">Confirmer mot de passe : </label>
-          <input className="input" />
+          <input required={currentStep === 4 ? true : false} onChange={(e)=>setCfrm(e.target.value)} className="input" />
         </div>
       </div>
     ];
 
-    const handleNext = () => {
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
         if (currentStep < steps.length - 1) {
           setCurrentStep(currentStep + 1);
         }
-      };
+      }
     
-      const handleBack = () => {
+      const handleBack = (e) => {
+        e.preventDefault();
         if (currentStep > 0) {
           setCurrentStep(currentStep - 1);
         }
@@ -97,13 +145,13 @@ export default function Reg() {
       <div className="progressBar">
         <div className="d-flex flex-column align-items-center">
           Nom
-          <div className="step activeStep">1</div>
+          <div className={currentStep >= 1 ? "step activeStep" : "step"}>1</div>
           Contact
-          <div className="step">2</div>
+          <div className={currentStep >= 2 ? "step activeStep" : "step"}>2</div>
           Naissance
-          <div className="step">3</div>
+          <div className={currentStep >= 3 ? "step activeStep" : "step"}>3</div>
           Situation
-          <div className="step">4</div>
+          <div className={currentStep >= 4 ? "step activeStep" : "step"}>4</div>
           Soumettre
           <div className="stepl">5</div>
         </div>
@@ -111,8 +159,9 @@ export default function Reg() {
 
 
 
-
+     <form onSubmit={(e)=>handleSubmit(e)}>
       <div className="regDiv">
+        
         <h1 className="ttl">Inscription</h1>
 
 
@@ -126,11 +175,12 @@ export default function Reg() {
 
         <div className="buttonsDiv">
           <button onClick={handleBack} disabled={currentStep === 0} className="retourBtn">Retour</button>
-          <button onClick={handleNext} disabled={currentStep === steps.length - 1} className="continuerBtn">
+          <button type="submit" disabled={currentStep === steps.length - 1} className="continuerBtn">
           {currentStep === steps.length -1 ? "Submit" : "Continuer"}
           </button>
         </div>
       </div>
+      </form>
     </div>
   );
 }
