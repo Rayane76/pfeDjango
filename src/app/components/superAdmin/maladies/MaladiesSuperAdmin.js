80cbdef7e7ce0b1a5258792489b96202377a90c8
Rayane <rayanehiouani.rh@gmail.com>
2024-05-31 20:57:22 +0100

    finished add maladie

--- src/app/components/superAdmin/maladies/MaladiesSuperAdmin.js
+++ src/app/components/superAdmin/maladies/MaladiesSuperAdmin.js
@@ -7,9 +7,13 @@ import Autocomplete from "@mui/material/Autocomplete";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import DeleteIcon from "@mui/icons-material/Delete";
+import axiosService from "@/app/helpers/axios";
+import { useRouter } from "next/navigation";
 
 
 export default function MaladiesSuperAdmin({maladies}){
+
+    const router = useRouter();
 
     const [filteredCat,setFilteredCat] = useState(undefined);
 
@@ -19,12 +23,63 @@ export default function MaladiesSuperAdmin({maladies}){
   const uniqueIsChronique = ["true","false"];
 
 
-  const handleDelete = (maladie) => {
-    
+  const [modalShowAdd,setModalShowAdd] = useState(false);
+
+  const [newMaladie,setNewMaladie] = useState({
+    name: "",
+    maladie_type: "",
+    isChronic: false, 
+  })
+
+
+  const handleAddMaladie = async () => {
+    await axiosService.post(`maladies/`,newMaladie)
+    .then((res)=>{
+       setModalShowAdd(false); 
+       router.refresh();
+    })
+    .catch((err)=>{
+       console.log(err);
+    })
+  }
+
+
+  const handleDelete = async (maladie) => {
+     await axiosService.delete(`maladies/${maladie.id}`)
+     .then((res)=>{
+        router.refresh();
+     })
+     .catch((err)=>{
+        console.log(err);
+     })
   }
 
     return(
         <div className="demandesPage">
+
+        <div className="d-flex justify-content-between mt-4 ms-4 me-4">
+       <h1>Maladies</h1>
+       <button
+          onClick={() => setModalShowAdd(true)}
+          title="Add"
+          className="cssbuttons-io-button"
+          id="nouveauBtnRadios"
+        >
+          <svg
+            height="25"
+            width="25"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path
+              d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          <span>Nouveau</span>
+        </button>
+        </div>
         
         <div className="historiqueFilterDiv">
         <Autocomplete
@@ -113,6 +168,59 @@ export default function MaladiesSuperAdmin({maladies}){
       
         </div>
         </div>
+        <Modal
+      show={modalShowAdd}
+      onHide={() => setModalShowAdd(false)}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Maladie
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Ajouter nouvelle maladie</h4>
+          <div className="d-flex flex-column gap-3">
+            <div className="d-flex">
+              <label className="me-2">Maladie : </label>
+              <input
+                name="name"
+                type="text"
+                onChange={(e)=>setNewMaladie((prev)=>({...prev,name:e.target.value}))}
+                required
+                placeholder="maladie ..."
+              ></input>
+            </div>
+            <div className="d-flex">
+              <label className="me-2">Categorie : </label>
+              <input
+                name="maladie_type"
+                onChange={(e)=>setNewMaladie((prev)=>({...prev,maladie_type:e.target.value}))}
+                type="text"
+                required
+                placeholder="maladie_type"
+              ></input>
+            </div>
+            <div className="d-flex">
+              <label className="me-2">Chronique ? : </label>
+              <input
+                name="isChronic"
+                onChange={(e)=>setNewMaladie((prev)=>({...prev,isChronic:e.target.checked}))}
+                type="checkbox"
+                required
+              ></input>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={()=>handleAddMaladie()}>Ajouter</Button>
+          <Button variant="secondary" onClick={() => setModalShowAdd(false)}>
+            Annuler
+          </Button>
+        </Modal.Footer>
+      </Modal>
         </div>
     )
 }
